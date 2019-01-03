588f67a94418e07cbc40f94c128f9529183a7267
Mark Nelson <mark.x.nelson@oracle.com>
2019-01-03 15:12:17 -0500

    change the worker security list to allow all traffic

--- network/vcn/securitylists.tf
+++ network/vcn/securitylists.tf
@@ -109,7 +109,7 @@ resource "oci_core_security_list" "K8SMasterSubnet" {
     },
     {
       protocol = "all"
-      source   = "${lookup(var.bmc_ingress_cidrs, "VCN-CIDR")}"
+      source   = "0.0.0.0/0"
     },
     {
       tcp_options {
